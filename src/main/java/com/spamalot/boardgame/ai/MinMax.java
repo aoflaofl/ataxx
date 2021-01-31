61669cb9c2b8ee322b613dc2dbc7976390c588a6
Gene Johannsen <aofl@yahoo.com>
2021-01-30 16:54:52 -0800

    Fix generic problem.

--- src/main/java/com/spamalot/boardgame/ai/MinMax.java
+++ src/main/java/com/spamalot/boardgame/ai/MinMax.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MinMax<P extends Position, M extends Move> {
+public class MinMax<P extends Position<M>, M extends Move<?>> {
   /** Logger for this class. */
   private static final Logger logger = LoggerFactory.getLogger(MinMax.class);
 
