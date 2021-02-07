614aadef473404f7b3c9054a0ba09a7320c81c28
Gene Johannsen <aofl@yahoo.com>
2021-02-06 18:27:01 -0800

    Unit test for MoveStack.

--- src/test/java/com/spamalot/boardgame/moves/MoveStackTest.java
+++ src/test/java/com/spamalot/boardgame/moves/MoveStackTest.java
@@ -1,29 +1,86 @@
 package com.spamalot.boardgame.moves;
 
 import com.spamalot.boardgame.board.Cell;
-
+import com.spamalot.boardgame.pieces.Color;
+import com.spamalot.boardgame.pieces.Piece;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MoveStackTest {
   @Test
   public void f() {
-    
-    MoveStack<Move> s = new MoveStack<>();
+
+    MoveStack<TestMove> s = new MoveStack<>();
+
+    TestMove m = new TestMove();
+
+    s.push(m);
+    System.out.println(s);
+
+    Assert.assertTrue(s.toString().contains("...."));
+
+    TestMove m3 = new TestMove();
+    s.push(m3);
+    Assert.assertFalse(s.toString().contains("...."));
+
+    s.pop();
+    TestMove m2 = s.pop();
+
+    Assert.assertEquals(m, m2);
+
   }
-  
-  class TestMove implements Move {
+
+  class TestMove implements Move<TestCell> {
 
     @Override
-    public Cell getFromCell() {
+    public TestCell getFromCell() {
       // TODO Auto-generated method stub
       return null;
     }
 
     @Override
-    public Cell getToCell() {
+    public TestCell getToCell() {
       // TODO Auto-generated method stub
       return null;
     }
-    
+
+  }
+
+  class TestCell implements Cell<TestPiece> {
+
+    @Override
+    public void putDownPiece(TestPiece p) {
+      // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public TestPiece getPiece() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    @Override
+    public TestPiece pickUpPiece() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+      // TODO Auto-generated method stub
+      return false;
+    }
+
+  }
+
+  class TestPiece implements Piece {
+
+    @Override
+    public Color getColor() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
   }
 }
